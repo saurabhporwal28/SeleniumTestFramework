c2d32414e5c1decd4f156341716b4571774b5a77
Saurabh Porwal <saurabh.028@gmail.com>
2018-03-13 12:44:09 -0400

    adding solution to repo

---
+++ SeleniumTestFramework/TestWithChrome.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+
+namespace SeleniumTestFramework
+{
+    class TestWithChrome
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void StartBrowser()
+        {
+            driver = new ChromeDriver("..\\Automation\\chromedriver");
+        }
+
+        [Test]
+        public void TestRun()
+        {
+            driver.Url = "http://www.google.co.in";
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            driver.Close();
+        }
+    }
+}
